1a48a2018ea806b2c891da7c9d728774b2f65fbc
shahroz.khan <shehroz.altaf92@gmail.com>
2019-11-07 18:33:40 +0500

    Initial Commit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,6 +1,6 @@
 <h1>Welcome
     <?php if (isset($_SESSION['login']['idUser']) && $_SESSION['login']['idUser'] != '') {
-        echo $_SESSION['login']['idUser'];
+        echo $_SESSION['login']['username'];
     } else {
         echo 'No Login';
     } ?>
